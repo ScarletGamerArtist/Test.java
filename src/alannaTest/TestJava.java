e1730fcc6e1c1e3864303f73dc81fdba0893bd35
123al <123al@10.0.0.238>
2018-09-11 21:12:11 -0600

    This is a test.

---
+++ src/alannaTest/TestJava.java
@@ -0,0 +1,15 @@
+package alannaTest;
+/*
+ * Alanna Botscharow
+ * 9/18
+ * This is just a test program.
+ */
+public class TestJava {
+
+	public static void main(String[] args) {
+		// Will display the text, "This is a test." 
+		System.out.print("This is a test.");
+
+	}
+
+}
